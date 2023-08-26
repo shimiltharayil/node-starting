4d5343b6c1fe2de9a8e68a5b993319e013cff21e
Shimil Tharayil <109408967+shimiltharayil@users.noreply.github.com>
2023-08-26 21:05:10 +0530

    added routes

--- index.ts
+++ index.ts
@@ -1,14 +1,20 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import adminRouter from "./routes/admin.route";
+import shopRouter from "./routes/shop.route";
 
 dotenv.config({ path: ".env" });
 
 const app: Express = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 const port = process.env.PORT;
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
+
+// Routes
+
+app.use("/admin", adminRouter);
+app.use("/shop", shopRouter);
